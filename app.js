4322a01fc9ca351eccd5ad249f19006f8998b4bf
zeroxy <krokrow@gmail.com>
2015-05-12 08:30:38 +0000

    init project
    crawling the menu data of SDS

---
+++ app.js
@@ -0,0 +1,112 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var cheerio = require('cheerio');
+
+var dela = {
+  getlunchMenu : function (cb){
+    var options1 = {
+      host: "www.sdsfoodmenu.co.kr",
+      port: 9106,
+      path: "/foodcourt/menuplanner/list?zoneId=ZONE01"
+    };
+    
+    var options2 = {
+      host: "www.sdsfoodmenu.co.kr",
+      port: 9106,
+      path: "/foodcourt/menuplanner/list?zoneId=ZONE02"
+    };
+    
+    var lunchmenu=[];
+    var html1 ="";
+    var html2 ="";
+    var req1 = http.request(options1, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        html1+=chunk;
+      });
+      res.on('end', function() {
+        $=cheerio.load(html1);
+        $('td').each(function(i,td){
+          if(i!==0){
+            var children = $(this).children();
+            if(children.length==7){
+              lunchmenu.push({
+                "floor":"B1",
+                "name":children[0].children[0].data,
+                "enname":children[2].children[0].data,
+                "kcal":children[4].children[0].data,
+                "won":children[6].children[0].data
+              });
+            }
+          }
+        });
+        req2.end();
+      });
+    });
+    var req2 = http.request(options2, function(res) {
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        html2+=chunk;
+      });
+      res.on('end', function() {
+        $=cheerio.load(html2);
+        $('td').each(function(i,td){
+          if(i!==0){
+            var children = $(this).children();
+            if(children.length==7){
+              lunchmenu.push({
+                "floor":"B2",
+                "name":children[0].children[0].data,
+                "kcal":children[4].children[0].data,
+                "won":children[6].children[0].data
+              });
+            }
+          }
+        });
+        cb(lunchmenu);
+      });
+    });
+    req1.on('error', function(e) {
+      console.log('problem with request1: ' + e.message);
+    });
+    req2.on('error', function(e) {
+      console.log('problem with request2: ' + e.message);
+    });
+    req1.end();
+  }
+};
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', function(req, res){
+  dela.getlunchMenu(function(data){ res.send(data);});
+});
+var server = http.createServer(app)
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'), app.get('ip'));
+});
